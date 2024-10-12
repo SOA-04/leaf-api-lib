292e4cc88200cd4edf1bb45828f4080994079dbc
Harvey-125 <sideman8787@gmail.com>
2024-10-12 16:20:29 +0800

    Create nominatim_api_spec.rb  by Harvey

---
+++ spec/api/nominatim_api_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'minitest/rg'
+require 'yaml'
+
+require_relative '../../lib/services/nominatim_api'
+require_relative '../../lib/services/api_errors'
+
+CORRECT_RESPONSE = YAML.safe_load_file('spec/fixtures/nominatim_serach_nthu-results.yaml')
+
+describe 'Test Nominatim API library' do
+  describe 'API Search Succeed' do
+    it 'Receive correct data for NTHU search.' do
+      nominatim_api = NominatimAPI.new
+      payload = nominatim_api.search('清華大學')
+      _(payload[0]['place_id']).must_equal CORRECT_RESPONSE[0]['place_id']
+      _(payload[0]['name']).must_equal CORRECT_RESPONSE[0]['name']
+      _(payload[0]['lat']).must_equal CORRECT_RESPONSE[0]['lat']
+      _(payload[0]['lon']).must_equal CORRECT_RESPONSE[0]['lon']
+      _(payload[1]['place_id']).must_equal CORRECT_RESPONSE[1]['place_id']
+      _(payload[1]['name']).must_equal CORRECT_RESPONSE[1]['name']
+      _(payload[1]['lat']).must_equal CORRECT_RESPONSE[1]['lat']
+      _(payload[1]['lon']).must_equal CORRECT_RESPONSE[1]['lon']
+    end
+  end
+
+  describe 'API Authentication Failed' do
+    it 'Raise errors when provided with an invalid query' do
+      nominatim_api = NominatimAPI.new
+      _(proc do
+        nominatim_api.search('')
+      end).must_raise HTTPError
+    end
+  end
+end
