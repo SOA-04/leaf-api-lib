9eca45af1a75d5819bbf4446d3b2f2430d7d67a7
pedestrianlove <37164678+pedestrianlove@users.noreply.github.com>
2024-10-20 23:37:32 +0800

    Revert "App mvc wuyuwin"

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -2,14 +2,12 @@
 
 require 'roda'
 require 'slim'
-require 'yaml'
-require_relative '../models/entities/trip'
-require_relative '../models/mappers/trip_mapper'
+require_relative '../models/mappers/location_mapper'
 require_relative '../models/gateways/google_maps_api'
 require_relative '../../config/environment'
 
 module LeafAPI
-  # This is the main application class that handles routing in LeafAPI
+  # Web App
   class App < Roda
     plugin :render, engine: 'slim', views: 'app/views'
     plugin :assets, css: 'style.css', path: 'app/views/assets'
@@ -17,7 +15,7 @@ module LeafAPI
     plugin :halt
 
     route do |routing|
-      routing.assets
+      routing.assets # load CSS
       response['Content-Type'] = 'text/html; charset=utf-8'
       setup_routes(routing)
     end
@@ -38,69 +36,54 @@ module LeafAPI
 
     def setup_location_routes(routing)
       routing.on 'locations' do
+        setup_location_search(routing)
+        setup_location_form(routing)
+        setup_location_result(routing)
+      end
+    end
+
+    def setup_location_search(routing)
+      routing.post 'search' do
+        handle_search(routing)
+      end
+    end
+
+    def setup_location_form(routing)
+      routing.is do
         routing.get do
-          view 'locations'
+          view 'location_form'
         end
       end
+    end
+
+    def setup_location_result(routing)
+      routing.on String do |location_query|
+        routing.get do
+          handle_location_query(location_query)
+        end
+      end
+    end
+
+    def handle_search(routing)
+      location_query = routing.params['location'].downcase
+      routing.redirect "/locations/#{CGI.escape(location_query)}"
+    end
+
+    def handle_location_query(location_query)
+      location_entity = LeafAPI::GoogleMaps::LocationMapper.new(
+        LeafAPI::GoogleMaps::API,
+        CONFIG['GOOGLE_TOKEN']
+      ).find(location_query)
+
+      view 'location_result', locals: { location: location_entity }
     end
 
     def setup_trip_routes(routing)
       routing.on 'trips' do
-        setup_trip_submit(routing)
-
-        setup_trip_form(routing)
-
-        setup_trip_result(routing)
-      end
-    end
-
-    def setup_trip_submit(routing)
-      routing.post 'submit' do
-        routing.redirect "#{routing.params['origin']}/#{routing.params['destination']}/#{routing.params['strategy']}"
-      end
-    end
-
-    def setup_trip_form(routing)
-      routing.is do
         routing.get do
-          view 'trip_form'
+          view 'trip'
         end
       end
-    end
-
-    def setup_trip_result(routing)
-      routing.on String, String, String do |origin, destination, strategy|
-        routing.get do
-          origin ||= '24.795707, 120.996393'
-          destination ||= '24.786930, 120.988428'
-          strategy ||= 'walking'
-          trip = trip_entities(origin, destination, strategy)
-
-          view 'trip_result', locals: { trip: trip }
-        end
-      end
-    end
-
-    def handle_submit(routing)
-      trip_params = extract_trip_data(routing)
-      routing.redirect "#{trip_params[:origin]}/#{trip_params[:destination]}/#{trip_params[:strategy]}"
-    end
-
-    def extract_trip_data(routing)
-      {
-        origin: CGI.escape(routing.params['origin']),
-        destination: CGI.escape(routing.params['destination']),
-        strategy: CGI.escape(routing.params['strategy'])
-      }
-    end
-
-    def trip_entities(origin, destination, strategy)
-      mapper = LeafAPI::GoogleMaps::TripMapper.new(
-        LeafAPI::GoogleMaps::API,
-        CONFIG['GOOGLE_TOKEN']
-      )
-
-      mapper.find(CGI.unescape(origin), CGI.unescape(destination), CGI.unescape(strategy))
     end
   end
 end
